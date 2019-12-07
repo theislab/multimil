b797c5383344ef307c6a792e4d446a90f21c7ca7
EliHei2 <eheidari@student.ethz.ch>
2019-12-07 12:10:51 +0100

    added peaks

--- code/rds_to_hd5.R
+++ code/rds_to_hd5.R
@@ -12,7 +12,6 @@ write.csv(rowData(scRNA_all), "data/scRNA/var.csv", row.names = F)
 write.csv(colData(scRNA_all), "data/scRNA/obs.csv")
 
 
-
 scATAC_all <- readRDS("data/scATAC-Cicero-GA-Hematopoiesis-MPAL-191120.rds")
 col_all <- colData(scATAC_all)
 scATAC_healthy <- readRDS("data/scATAC-Cicero-GA-Hematopoiesis-191120.rds")
@@ -20,5 +19,16 @@ col_healthy <- colData(scATAC_healthy)
 col_all[rownames(col_healthy), "ProjectClassification"] <- col_healthy$BioClassification
 colData(scATAC_all) <- col_all
 saveHDF5SummarizedExperiment(scATAC_all, "data/scATAC", replace = T)
+write.csv(rowData(scATAC_all), "data/scATAC/var.csv", row.names = F)
+write.csv(colData(scATAC_all), "data/scATAC/obs.csv")
+
+
+scATAC_all <- readRDS("data/scATAC-All-Hematopoiesis-MPAL-191120.rds")
+col_all <- colData(scATAC_all)
+scATAC_healthy <- readRDS("data/scATAC-Healthy-Hematopoiesis-191120.rds")
+col_healthy <- colData(scATAC_healthy)
+col_all[rownames(col_healthy), "ProjectClassification"] <- col_healthy$BioClassification
+colData(scATAC_all) <- col_all
+DropletUtils::write10xCounts(x = assay(scATAC_all), path = "data/scATAC")
 write.csv(rowData(scATAC_all), "data/scATAC/var.csv", row.names = F)
 write.csv(colData(scATAC_all), "data/scATAC/obs.csv")
