782525e142baa413b33d0f50cff812c9af85950a
Anastasia Litinetskaya <alitinet@gmail.com>
2020-11-07 13:17:14 +0100

    added setup.py

--- scmulti/__init__.py
+++ scmulti/__init__.py
@@ -2,3 +2,13 @@ from . import utils
 from . import models
 from . import datasets
 from . import metrics
+
+__author__ = ', '.join([
+    'Alireza Omidi',
+    'Anastasia Litinetskaya',
+    'Mohammad Lotfollahi',
+])
+
+__email__ = ', '.join([
+    'ga58som@mytum.de', #test
+])
